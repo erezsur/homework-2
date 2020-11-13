694ee21dd78df1065632ec054210ffd960a51929
erezsur <73992573+erezsur@users.noreply.github.com>
2020-11-13 10:37:16 +0200

    Add files via upload

---
+++ homework #2.py
@@ -0,0 +1,18 @@
+# functions
+def SongName():
+    print("Good Music")
+
+def Artist():
+    print("Ravid Plotnik")
+
+def Ganere():
+    print("Hip Hop")
+
+def Year():
+    num = 2014
+    print (num)
+#print
+SongName()
+Artist()
+Ganere()
+Year()
